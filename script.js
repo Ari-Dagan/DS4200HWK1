616f8267375c71d20277077fc414b9fa060c5f7c
aridagan12 <aridagan12@gmail.com>
2024-01-26 12:31:27 -0500

    Part 2 COMPLETE

--- script.js
+++ script.js
@@ -1,68 +1,68 @@
-// Changing the header 1
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "New Dog Title, he ate the other one";
 const myImage = document.querySelector("img");
+const myButton = document.querySelector("button");
 
-let dogFood;
-let nuggetAmt = 0
-let myButton = document.querySelector("button");
-let myHeading2 = document.querySelector("h2");
-/*
-This variable represent what I will feed my dog
-*/
-dogFood = "Chicken";
+let a = 20;
+let b = 20;
+let c = 20;
 
+let aVal= 10;
+let bVal = 20;
+let cVal = 30;
+let imgCounter = 1;
+let images = [
+    "images/me1.jpg",
+    "images/me2.png",
+    "images/me3.jpg",
+    "images/me4.JPG",
+    "images/me5.jpg",
+    "images/me6.jpg",
+    "images/me7.jpg",
+]
 
-function feedDog(food) {
-    nuggetAmt = nuggetAmt + food;
-    return nuggetAmt;
-  }
-
-function setFoodAmt() {
-const myName = prompt("Please enter what your feeding him");
-localStorage.setItem("Food", dogFood);
-myHeading2.textContent = `Big heading -  Da DAWG ATE THIS MUCH , ${dogFood}: ${nuggetAmt}`;
-}
-
-if (!localStorage.getItem("name")) {
-    feedDog(2);
-    setFoodAmt();
-  } else {
-    if (!myName) {
-        setFoodAmt();
-      }
-      else{
-    const storedName = localStorage.getItem("name");
-    myHeading2.textContent = `Big heading -  Da DAWG ATE THIS MUCH , ${dogFood}: ${nuggetAmt}`;
-      }
-  }
-
-feedDog(1);
-
-document.querySelector("html").addEventListener("click", function () {
-    alert("You fed my dog!");
-    feedDog(1);
-    if (nuggetAmt < 5) {
-        alert("Still Hungry");
-    } 
-    else {
-        alert("My Dog is Chillin now");
-        myHeading.textContent = "Full Boy";
-        myImage.setAttribute("src", "images/fullBoy.jpg");
-    }
-  });
-
-
-
-  myImage.onclick = () => {
+myImage.onclick = () => {
     const mySrc = myImage.getAttribute("src");
-    if (mySrc === "images/dogHungry.jpg") {
-      myImage.setAttribute("src", "images/fullBoy.jpg");
-    } else {
-      myImage.setAttribute("src", "images/dogHungry.jpg");
+    myImage.setAttribute("src", images[imgCounter]);
+    imgCounter += 1;
+    if (imgCounter == images.length) {
+        imgCounter = 0;
     }
-  };
+};
 
-  myButton.onclick = () => {
-    setFoodAmt();
-  };
+myButton.onclick = () => {
+    document.body.style.backgroundColor = 'rgb(' + a + ',' + b + ',' + c + ')';
+    if( a < 255) {
+        a += aVal;
+    }
+    else {
+        a = 10;
+        aVal +=30
+    }
+
+    if( b < 255) {
+        b += bVal;
+    }
+    else {
+        b = 10;
+        bVal +=20
+    }
+
+    if( c < 255) {
+        c += cVal;
+    }
+    else {
+        c = 10;
+        cVal +=10
+    }
+
+    if( cVal > 100) {
+        cVal = 5;
+    }
+
+    if( bVal > 100) {
+        bVal = 15;
+    }
+
+    if( aVal > 100) {
+        aVal = 25;
+    }
+};
